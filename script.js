773c954887d88a485e3c2a74d65b348679d4c5c3
Keenah <kvancamp@mail.com>
2022-05-30 18:41:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,22 @@ let currentRoundNumber = 1;
 // Write your code below:
 
 const generateTarget = () => {
-  Math.floor(Math.random() * 9);
+  return Math.floor(Math.random() * 9);
 }
+//added calculation to find the difference
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+    const humanDifference = Math.abs(targetGuess - humanGuess) //compare absolute value
+    const computerDifference = Math.abs(targetGuess - computerGuess)
+
+    return humanDifference <= computerDifference;
+  }
+  
+  const updateScore = winner => {
+    if (winner === 'human') {
+      humanScore++;
+    } else if (winner === 'computer') {
+      computerScore++;
+    }
+  }
+  
+  const advanceRound = () => currentRoundNumber++;
